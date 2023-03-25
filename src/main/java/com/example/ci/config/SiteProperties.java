32a7051addd363f687a8157ede34694bb7d32121
Kim woo jong <skekdnwhd@gmail.com>
2023-03-25 20:19:30 +0900

    chore: 로컬환경으로 테스트하기 위해3

---
+++ src/main/java/com/example/ci/config/SiteProperties.java
@@ -0,0 +1,23 @@
+package com.example.ci.config;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import org.springframework.stereotype.Component;
+
+@Getter
+@ToString
+@Component
+public class SiteProperties {
+    private final String authorName;
+    private final String authorEmail;
+
+    public SiteProperties(
+            @Value("${site.authorName}") String authorName,
+            @Value("${site.authorEmail}") String authorEmail) {
+        this.authorName = authorName;
+        this.authorEmail = authorEmail;
+    }
+}
---
+++ src/test/java/com/example/ci/config/SitePropertiesTest.java
@@ -0,0 +1,21 @@
+package com.example.ci.config;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SitePropertiesTest {
+
+    @Autowired
+    SiteProperties siteProperties;
+
+    @Test
+    void test() {
+        Assertions.assertEquals("Woo-local", siteProperties.getAuthorName());
+        Assertions.assertEquals("Woo@a.com-local", siteProperties.getAuthorEmail());
+    }
+}
