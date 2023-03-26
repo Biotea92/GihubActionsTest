06dba62331fa5a01460b6a9d3e22f40ca3c5be74
Kim woo jong <skekdnwhd@gmail.com>
2023-03-26 16:53:52 +0900

    Feat : HelloRestController

--- src/main/java/com/example/ci/controller/HelloController.java
+++ src/main/java/com/example/ci/controller/HelloController.java
@@ -4,8 +4,9 @@ import com.example.ci.config.SiteProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class HelloController {
 
